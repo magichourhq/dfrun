0b85c4f7bf9f087558ef670d78b739a15de1a634
David Hu <davidhu314@gmail.com>
2025-03-29 16:27:05 -0700

    chore: add release script (#2)

---
+++ release.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+unset GITHUB_TOKEN
+
+git checkout main
+git pull
+
+version=$(grep "^version =" Cargo.toml | sed 's/version = "\(.*\)"/\1/')
+
+type=$(gum choose 'patch' 'minor' 'major')
+
+# Split the version into major, minor, and patch
+IFS='.' read -r -a parts <<< "$version"
+major=${parts[0]}
+minor=${parts[1]}
+patch=${parts[2]}
+
+# Determine which part to bump
+case "$type" in
+    major)
+        major=$((major + 1))
+        minor=0
+        patch=0
+        ;;
+    minor)
+        minor=$((minor + 1))
+        patch=0
+        ;;
+    patch|*)
+        patch=$((patch + 1))
+        ;;
+esac
+
+# New version
+new_version="$major.$minor.$patch"
+
+function release() {
+    sed -i.bak "s/version = \"$version\"/version = \"$new_version\"/" Cargo.toml
+    rm *.bak
+    echo "Version bumped to $new_version"
+
+    git add Cargo.toml
+    git commit -m "Release: $new_version"
+    git push
+}
+
+gum confirm "Release $new_version?" && release
